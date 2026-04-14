1d6b4c0cc2fdf21f56b35894883b02be03603c8d
Zerba1986 <joazeirofelipe@gmail.com>
2026-04-14 10:05:57 -0300

    settings.js

---
+++ data/settings.js
@@ -0,0 +1,12 @@
+module.exports = {
+    uiPort: process.env.PORT || 3000,
+    uiHost: "0.0.0.0",
+
+    credentialSecret: "minha-chave-secreta",
+
+    editorTheme: {
+        projects: {
+            enabled: true
+        }
+    }
+}
