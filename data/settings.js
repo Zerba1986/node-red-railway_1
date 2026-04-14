0e07213b6171d927046ba2f45d4e7165427a83ef
Zerba1986 <joazeirofelipe@gmail.com>
2026-04-14 11:13:54 -0300

    Add admin authentication configuration

--- data/settings.js
+++ data/settings.js
@@ -4,6 +4,15 @@ module.exports = {
 
     credentialSecret: "minha-chave-secreta",
 
+    adminAuth: {
+        type: "credentials",
+        users: [{
+            username: "admin",
+            password: "$2b$08$K1Qp0lQx7QpZ8l8G5YzFJe7Yg3zVQ9b7xKQv1Y7JQ0z7Xz6zK1y6G",
+            permissions: "*"
+        }]
+    },
+
     editorTheme: {
         projects: {
             enabled: true
